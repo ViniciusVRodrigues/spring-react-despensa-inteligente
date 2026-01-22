17bba5627bd1996a68f1255f129666bd08f50d24
Vinícius Veiga Rodrigues <96784778+ViniciusVRodrigues@users.noreply.github.com>
2026-01-22 14:00:58 -0300

    Add files via upload

---
+++ edespensa/src/main/java/com/viniciusvr/edespensa/EdespensaApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusvr.edespensa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdespensaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdespensaApplication.class, args);
+	}
+
+}
---
+++ edespensa/src/test/java/com/viniciusvr/edespensa/EdespensaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusvr.edespensa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdespensaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
